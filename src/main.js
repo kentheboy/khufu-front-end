3747eb0dde54289f4e2eb9d0ae66d44299db2386
Ken Okpara <k_okpara-eze@itra.co.jp>
2023-09-16 23:59:01 +0900

    load global css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
+import globalCss from "./global.css";
 
 createApp(App)
-    .use(router)
+    .use(router, globalCss)
     .mount('#app')
