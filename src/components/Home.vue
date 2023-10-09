181f9559b326436d73d2eec398a31cd4e34a91c4
kentheboy <foryoukentheboy1020@gmail.com>
2023-10-09 20:11:45 +0900

    setup basic design of product-cards

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -71,6 +71,21 @@
         </div>
         <DateTimePicker></DateTimePicker>
         <Button text="空き状況を検索"></Button>
+        <div class="products">
+          <div class="product__card">
+            <img src="/img/main5@3x.fb64a8f0.png">
+            <div class="product__card--description">
+              <div class="product__card--description-title">ALPHARD</div>
+              <div class="product__card--description-passenger">乗車定員　7人</div>
+              <hr>
+              <div class="product__card--description-price">¥25,000~/24h</div>
+              <div class="product__card--description-select-buttons">
+                <Button text="会員の方"></Button>
+                <Button text="はじめての方"></Button>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section class="schedule" schedule-search-form>
         <div class="scheduleChild" />
@@ -104,10 +119,6 @@
         <div class="div163">(選択する)</div>
         <div class="div164">はじめての方</div>
         <img class="scheduleItem" alt="" src="/images/vector-2.svg" />
-        <button class="button3">
-          <div class="child" />
-          <div class="div4">空き状況を検索</div>
-        </button>
       </section>
       <div class="div35">
         <button class="button2">
@@ -463,7 +474,6 @@ export default {
 section {
   margin: 0 2.4rem;
 }
-
 .section {
   &__heroImageArea {
     margin: 0 0 1.2rem 0;
@@ -609,6 +619,74 @@ section {
     }
   }
 
+}
+.products{
+  display: flex;
+  justify-content: center;
+  .product__card {
+    border-radius: 3.49px;
+    background-color: var(--color-lightblue);
+    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.25);
+    width: 18.15rem;
+    height: 23.39rem;
+    display: inline-block;
+    img {
+      width: 18.15rem;
+      height: 14.06rem;
+    }
+    &--description {
+      position: relative;
+      height: 8rem;
+      &-title {
+        position: absolute;
+        top: 0;
+        left: 1rem;
+        font-size: var(--font-size-base);
+        letter-spacing: 0.1em;
+        color: var(--color-black);
+      }
+      &-passenger {
+        position: absolute;
+        top: 0.71rem;
+        right: 1rem;
+        letter-spacing: 0.15em;
+        display: inline-block;
+        width: 6.3rem;
+        height: 1.1rem;
+        color: var(--color-black);
+      }
+      hr {
+        position: absolute;
+        top: 1.7rem;
+        left: 6.5rem;
+        width: 30%;
+        border: solid 0.1px #c4cfd5;
+      }
+      &-price {
+        font-size: 1.2rem;
+        letter-spacing: 0.15em;
+        position: absolute;
+        top: 2.8rem;
+        right: 1rem;
+        color: var(--color-black);
+      }
+      &-select-buttons {
+        display: flex;
+        padding: 0 1.5rem;
+        justify-content: space-around;
+        position: relative;
+        top: 5.5rem;
+        &::v-deep button {
+          width: 7rem;
+          font-size: var(--font-size-5xs);
+          letter-spacing: 0.1em;
+          margin: 0;
+          box-shadow: 0.9974358678px 1.9948717356px 1.99px rgba(0, 0, 0, 0.25);
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 </style>
   
