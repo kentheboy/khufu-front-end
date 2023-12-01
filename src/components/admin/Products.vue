e8092034a7da2848d4f6ca276d31ad7020c9a5dc
kentheboy <foryoukentheboy>
2023-12-01 22:10:38 +0900

    add dialog close action

--- src/components/admin/Products.vue
+++ src/components/admin/Products.vue
@@ -33,7 +33,7 @@
             </Column>
         </DataTable>
         <Dialog 
-            v-model:visible="productDialog"
+            v-model:visible.sync="productDialog"
             maximizable
             header=""
             :modal="true"
@@ -120,6 +120,7 @@
                     autofocus
                 />
             </template>
+            <ProgressSpinner v-if="loading" />
         </Dialog>
         <Dialog
             v-model:visible="deleteProductDialog"
@@ -135,6 +136,7 @@
                 <Button label="削除" icon="pi pi-check" class="p-button-text" severity="danger" @click="sendDeleteProcudct" autofocus></Button>
             </template>
         </Dialog>
+        <Toast />
     </div>
 </template>
 
@@ -144,6 +146,7 @@ import Column from 'primevue/column';
 import Dialog from 'primevue/dialog';
 import Button from "primevue/button";
 import Badge from 'primevue/badge';
+import ProgressSpinner from 'primevue/progressspinner';
 import Input from "/src/components/common/form/Input";
 import axios from 'axios';
 
@@ -154,6 +157,7 @@ export default {
         Dialog,
         Button,
         Badge,
+        ProgressSpinner,
         Input
     },
     data() {
@@ -174,6 +178,7 @@ export default {
                 isSmokingAllowed: false
             },
             products: [],
+            loading: false,
             deleteProductId: null,
             productStatus: ["利用可","車検中","点検中"],
             productDialog: false,
@@ -274,7 +279,11 @@ export default {
                 "images": JSON.stringify(images)
             }
 
+            this.loading = true;
             await axios.post(`${this.backendDomain}/api/products/create`, data).then(() => {
+                this.displayDialog = false;
+                this.productDialog = false;
+                this.getProducts();
             })
         },
         async sendDeleteProcudct() {
