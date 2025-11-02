569f325cdbf14b0955324d768a9c0e5eedcb13a5
kentheboy <foryoukentheboy1020@gmail.com>
2025-11-02 18:10:08 +0800

    fix: update hero image file extensions for consistency

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -386,8 +386,8 @@ export default {
         "/images/hero-images/image1.jpg",
         "/images/hero-images/image2.jpg",
         "/images/hero-images/image3.jpg",
-        "/images/hero-images/image4.jpg",
-        "/images/hero-images/image5.png",
+        "/images/hero-images/image4.png",
+        "/images/hero-images/image5.jpg",
         "/images/hero-images/image6.jpg",
       ],
       heroImagesSp: [
